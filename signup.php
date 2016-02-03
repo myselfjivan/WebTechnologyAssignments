f9d746af853590dd62be12d0dc64658822094cf2
jivan <jivan@mint>
2016-02-03 11:06:05 +0530

    removed mysql -- storing into session only

--- signup.php
+++ signup.php
@@ -1,30 +1,18 @@
 <?php
-   include("config.php");
+   //include("config.php");
    include("header.php");
    session_start();
-   
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      
-      $sql = "SELECT id FROM Users WHERE username = '$myusername' and password = '$mypassword'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
-      
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      if($count == 1) {
-         //session_register("myusername");
-         //$_SESSION['login_user'] = $myusername;
-         
-         header("location: index.php");
-      }else {
-         $error = "Your Login Name or Password is invalid";
+   if(isset($_SESSION['userinfo'])){
+       header("location: index.php");
+   }else if($_SERVER["REQUEST_METHOD"] == "POST") {
+       $username = $_POST("username");
+       $password = $_POST("password");
+       if($username == "jivan" && $password == "test"){
+           $userinfo['username'] = $username;
+           $_SESSION['userinfo'] = $userinfo;
+          header("location: index.php");
+      }else{
+          $error = "Your Login Name or Password is invalid";
       }
    }
 ?>
@@ -45,9 +33,9 @@
                              <p>If you have previously Login with us, <a href="#">Click Here</a></p>
                              <form action="" method="POST">
                                  <h5>User Name:</h5>	
-                                 <input type="text" value="">
+                                 <input type="text" name="username" value="" required="">
                                  <h5>Password:</h5>
-                                 <input type="password" value=""><br>					
+                                 <input type="password" name="password" value="" required=""><br>					
                                  <input type="submit" value="Login">
 
                              </form>
